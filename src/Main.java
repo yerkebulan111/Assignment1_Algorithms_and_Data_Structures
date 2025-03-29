eb3f7c9eca03ac7495b79191293597a4b8431796
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-03-29 06:14:44 +0500

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         double startTime = System.nanoTime();
-        int result = fibb();
+        int result = aPowerOfn();
         double endTime = System.nanoTime();
         double duration = (endTime - startTime) / 1000000;
-        System.out.println("The 17-th element in Fibonacci sequence is: " + result);
+        System.out.println("2 to the power of 10 is: " + result);
         System.out.println("Time taken: " + duration + "milliseconds");
+    }
+
+
+
+    /**
+     * This method returns 2^10
+     * It is a convenience method that internally calls aPowerOfn(int a, int n) method
+     *
+     * @return 2^10
+     */
+    public static int aPowerOfn() {
+        return aPowerOfn(2,10);
+    }
+
+    /**
+     * The method calculates a to the power of n: a^n
+     * It uses a recursive approach
+     * Time complexity: O(n), where n is a given power
+     * The method breaks down the task into smaller tasks: a^n = a*a^(n-1),
+     * a^(n-1) = a*a*a(n-2), and so on, when it comes a^1 where n==1, it returns a
+     *
+     * @param a The number which will be calculated its n power
+     * @param n Power of the a
+     * @return The a to the power of n
+     */
+    public static int aPowerOfn(int a, int n) {
+        if (n==1) {return a;}
+        else {return a * aPowerOfn(a, n-1);}
     }
 
 
