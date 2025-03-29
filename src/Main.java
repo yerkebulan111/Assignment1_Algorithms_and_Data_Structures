a6857009d67d1e151ab84b2298076cc62977e2c5
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-03-29 05:51:38 +0500

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,43 @@
 public class Main {
     public static void main(String[] args) {
         double startTime = System.nanoTime();
-        int result = facto();
+        int result = fibb();
         double endTime = System.nanoTime();
         double duration = (endTime - startTime) / 1000000;
-        System.out.println("The factorial of 7 is: " + result);
+        System.out.println("The 17-th element in Fibonacci sequence is: " + result);
         System.out.println("Time taken: " + duration + "milliseconds");
     }
+
+
+
+    /**
+     * This function gives 17-th element in Fibonacci sequence
+     * It is a convenience method that internally calls fibb(int n) method
+     *
+     * @return Element which is in 17-th position in Fibonacci sequence
+     */
+    public static int fibb() {
+        return fibb(17);
+    }
+
+    /**
+     * This function n-th element in Fibonacci sequence
+     * It uses a recursive approach
+     * Time complexity: O(2^n), where n is a given number
+     * The method breaks down the task into smaller tasks: fibb(n) = fibb(n-1) + fibb(n-2),
+     * fibb(n-1) = fibb(n-2) + fibb(n-3) and so on,
+     * everytime there is block fibb(0) or fibb(1) in STACK, it returns 0 or 1 respectively
+     *
+     * @param n n-th position in Fibonacci sequence which will be calculated
+     * @return n-th element in the Fibonacci sequence
+     */
+    public static int fibb(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        else return fibb(n - 1) + fibb(n - 2);
+    }
+
 
 
 
@@ -14,7 +45,7 @@ public class Main {
      * This method calculates factorial of 7
      *It is a convenience method that internally calls the facto(int n) method with default value of 7
      *
-     * @return
+     * @return Factorial of 7
      */
     public static int facto() {
         return facto(7);
