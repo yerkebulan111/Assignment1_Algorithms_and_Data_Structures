97f85b8190418bee0e02ae3944bc0444c460a3a9
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-03-29 05:29:32 +0500

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         double startTime = System.nanoTime();
-        String result = isItPrime() ? "Prime" : "Composite";
+        int result = facto();
         double endTime = System.nanoTime();
         double duration = (endTime - startTime) / 1000000;
-        System.out.println("7 is: " + result);
+        System.out.println("The factorial of 7 is: " + result);
         System.out.println("Time taken: " + duration + "milliseconds");
+    }
+
+
+
+    /**
+     * This method calculates factorial of 7
+     *It is a convenience method that internally calls the facto(int n) method with default value of 7
+     *
+     * @return
+     */
+    public static int facto() {
+        return facto(7);
+    }
+
+    /**
+     * This method calculates factorial of a given number
+     * It uses a recursive approach
+     * Time complexity: O(n), where n is the parameter of this function
+     * This function calculates this task by dividing (divide & conquer) it into smaller parts: facto(n) = n*facto(n-1)
+     *facto(n-1) = (n-1)*facto(n-2) and so on
+     *
+     * @param n The number that will be calculated its factorial
+     * @return Factorial of n
+     */
+    public static int facto(int n) {
+        if (n == 1) return 1;
+        else return n * facto(n - 1);
     }
 
 
